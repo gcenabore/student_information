776fed075883f4d7396625b0bc14e1b52df140e6
gcenabore <gianenabore30@gmail.com>
2024-10-22 21:44:31 +0800

    pseudocode

---
+++ student_info.py
@@ -0,0 +1,8 @@
+    #Initialize an empty array to store entries (name, age)
+    #define validation functions (valid name, valid age)
+    #ask the user to input name and age
+    #if the input is valid, store it in the array, otherwise show an error message
+    #after each valid entries, ask if the user wants to add another entry
+    #continue the loop until the user says "No"
+    #when finished, find and display the oldest person in the entry
+
